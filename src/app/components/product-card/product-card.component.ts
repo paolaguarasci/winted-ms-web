db4a44e61af6db2fb6cac5b1a92073f3eb55c56d
Paola Guarasci <paolaguarasci@gmail.com>
2023-05-15 09:07:01 +0200

    pare che va

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,28 +1,28 @@
 import { Component, Input, OnInit } from '@angular/core';
 
-interface User {
-  name: string,
-  image: string
-}
-
-interface Product{
-  img: string,
-  price: string,
-  prefered: number,
-  size: string,
-  brand: string,
-  user: User
-}
+import { CurrencyPipe } from '@angular/common';
+import { Product } from 'src/app/models/Product';
 
 @Component({
   selector: 'product-card',
+  providers: [CurrencyPipe],
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
 })
 export class ProductCardComponent implements OnInit {
   @Input() product!: Product;
   @Input() showUser!: boolean;
-
+  constructor(private currencyPipe: CurrencyPipe) {}
   ngOnInit(): void {
+    if (!this.product.featured && !this.product.resources) {
+      this.product.featured = 'https://fakeimg.pl/200x300';
+    } else if (!this.product.featured && this.product.resources.length > 0) {
+      this.product.featured =
+        'http://localhost:8080/api/v1/resource/image/' +
+        this.product.resources[0];
+    }
+    let originalPrice = parseFloat(this.product.price);
+    this.product.price =
+      this.currencyPipe.transform(originalPrice, 'EUR', 'symbol', '.2') ?? '';
   }
 }
