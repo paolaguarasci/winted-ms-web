8fd88246013f578addc1cdfb31e2156404e9497d
Paola Guarasci <paolaguarasci@gmail.com>
2023-05-20 16:11:37 +0200

    sa

--- src/app/models/Order.ts
+++ src/app/models/Order.ts
@@ -1,6 +1,13 @@
 import { Resource } from './Resource';
 
 export class Order extends Resource<Order> {
+  public buyer!: string;
+  public product!: string;
+  public owner!: string;
+  public status!: string;
+  public address!: string;
+  public paymendMethod!: string;
+
   constructor(model?: Partial<Order>) {
     super(model);
   }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -5,16 +5,13 @@ import { ResourceService } from './resource.service';
 import { OffertResponse } from '../models/OffertResponse';
 import { Observable } from 'rxjs';
 import { Order } from '../models/Order';
+
 @Injectable({ providedIn: 'root' })
 export class OrderService extends ResourceService<Order> {
   constructor(private http: HttpClient) {
-    super(http, Inbox, `http://localhost:8080/api/v1/order`);
+    super(http, Order, `http://localhost:8080/api/v1/order`);
   }
-
-  public buy(productId): Observable<Order> {
-    return this.http.post<Order>(this.apiUrl, {
-      product: productId
-    });
+  public buy(order: Order): Observable<Order> {
+    return this.http.post<Order>(this.apiUrl, order);
   }
-
 }
