b3067e9fc6fe444424608587978e821f716acb97
Paola Guarasci <paolaguarasci@gmail.com>
2023-07-08 13:13:18 +0200

    redirect nella auth guard

--- src/app/guards/canactivate.guard.ts
+++ src/app/guards/canactivate.guard.ts
@@ -1,17 +1,23 @@
-import { CanActivateFn } from '@angular/router';
-import {Router} from '@angular/router';
-import {tap} from 'rxjs';
-import {inject} from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { CanActivateFn } from '@angular/router';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
+import { tap } from 'rxjs';
 
 export const canactivateGuard: CanActivateFn = (route, state) => {
-  console.log("ciao sono nella canactivateGuard")
+  console.log('ciao sono nella canactivateGuard');
 
   const router = inject(Router);
-  const service = inject(AuthService)
+  const service = inject(AuthService);
 
   if (service.checkCredentials()) {
     return true;
   }
-  return router.createUrlTree(['/']);
+  // return router.createUrlTree(['/']);
+
+  // not logged in so redirect to login page with the return url
+  router.navigate(['login'], {
+    queryParams: { redirectUrl: "https://localhost:4200" + state.url },
+  });
+  return false;
 };
